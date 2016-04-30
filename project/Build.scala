83bcdb18401def990576597c6cb400553268bd00
Julio CN Souza <juliocnsouzadev@gmail.com>
2016-04-30 19:56:39 -0300

    main classes

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import com.typesafe.sbt.SbtScalariform.ScalariformKeys
  */
 object Build extends sbt.Build {
 
-  lazy val root = Project("SampleProject", file("."))
+  lazy val root = Project("BuildingAGraphWithSpark", file("."))
     .settings(basicSettings: _*)
     .settings(libraryDependencies ++= Dependencies.basic)
 
@@ -21,7 +21,9 @@ object Build extends sbt.Build {
     resolvers ++= Seq(
       "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases",
       "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-      "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"))
+      "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
+    )
+  )
 
   // scalariform code format settings
   SbtScalariform.scalariformSettings // enable scalariformSettings
---
+++ src/main/scala/br/com/juliocnsouza/graphwithspark/PlaceNode.scala
@@ -0,0 +1,7 @@
+package br.com.juliocnsouza.graphwithspark
+
+class PlaceNode extends Serializable {
+
+  val name: String;
+
+}
